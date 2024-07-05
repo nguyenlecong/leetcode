5e417f079397fab2811bdfc69d359142d91a1cbe
nguyenlecong <nguyen.lc41@gmail.com>
2024-07-05 10:27:09 +0700

    1247

---
+++ 1247.minimum-swaps-to-make-strings-equal.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1247 lang=python3
+#
+# [1247] Minimum Swaps to Make Strings Equal
+#
+
+# @lc code=start
+class Solution:
+    def minimumSwap(self, s1: str, s2: str) -> int:
+        a = 0
+        b = 0
+        for i in range(len(s1)):
+            if s1[i] == 'x' and s2[i] == 'y':
+                a += 1
+            elif s1[i] == 'y' and s2[i] == 'x':
+                b += 1
+        if a % 2 == 0 and b % 2 == 0:
+            return a // 2 + b // 2
+        elif a % 2 == 1 and b % 2 == 1:
+            return a // 2 + b // 2 + 2
+        else:
+            return -1
+# @lc code=end
+
